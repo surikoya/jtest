6392bf5d2283a47318c5b19c58c19feac9605ee1
Suresh Koya <skoya@vmtechllc.com>
2018-12-31 18:38:13 -0800

    Master file

---
+++ src/main/java/com/vmtechllc/App.java
@@ -0,0 +1,19 @@
+package com.vmtechllc;
+
+import java.util.Map;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+	
+	private void myVeryPrivateMethod() {
+	}
+}
